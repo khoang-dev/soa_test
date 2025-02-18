3bd2f4a8edf456e8c99c16ef10a385e9b321b691
kvd97 <khoangvo.actsone@gmail.com>
2025-02-18 16:32:14 +0700

    feat: ✨ update header

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,9 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header
-    class="fixed top-0 left-0 right-0 flex items-center justify-between p-4 bg-gray-800 text-white z-10 shadow-lg"
+    class="flex items-center justify-between p-2 bg-gray-800 text-white z-10 shadow-lg w-full header"
   >
     <div class="flex items-center">
-      <img alt="Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
       <div class="ml-4 text-lg font-bold">LOGO SAMPLE</div>
     </div>
 
@@ -22,7 +21,11 @@ import { RouterLink, RouterView } from 'vue-router'
       <img src="@/assets/icons/Mountains.svg" alt="Icon" class="w-6 h-6" />
       <img src="@/assets/icons/Fishing.svg" alt="Settings" class="w-6 h-6" />
       <img src="@/assets/icons/Crosshair.svg" alt="External Link" class="w-6 h-6" />
-      <button class="bg-orange-500 text-white px-4 py-2 rounded">Action</button>
+      <button
+        class="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 cursor-pointer w-10 h-10 flex items-center justify-center"
+      >
+        <img src="@/assets/icons/ArrowUpRight.svg" alt="Search" class="w-6 h-6" />
+      </button>
     </div>
   </header>
 
@@ -33,5 +36,17 @@ import { RouterLink, RouterView } from 'vue-router'
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+.header {
+  border-bottom: 1px solid rgba(238, 238, 238, 0.2);
+
+  background: rgba(86, 44, 44, 0.7);
+
+  backdrop-filter: blur(7.5px);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
 }
 </style>
